d79ec5803c22105846cb28bc98a081a43a8f2c09
Dmitriy Nekliudov <intknwn@gmail.com>
2023-01-13 18:00:09 +0300

    feat: add events service

---
+++ pages/event/_id.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow"> @{{ event.time }} on {{ event.date }} </span>
+      <h1 class="title">
+        {{ event.title }}
+      </h1>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+
+    <span name="map">
+      <h2>Location</h2>
+    </span>
+
+    <address>{{ event.location }}</address>
+
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">
+        {{ event.attendees ? event.attendees.length : 0 }}
+      </span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  fetch({ error, store, params }) {
+    try {
+      store.dispatch('events/fetchEvent', params.id)
+    } catch (e) {
+      error({
+        statusCode: e.statusCode,
+        message: 'Unable to fetch event at this time. Please try again.',
+      })
+    }
+  },
+  head() {
+    return {
+      title: this.event.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `What you need to know about event ${this.event.title}`,
+        },
+      ],
+    }
+  },
+  computed: {
+    ...mapState('events', ['event']),
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
